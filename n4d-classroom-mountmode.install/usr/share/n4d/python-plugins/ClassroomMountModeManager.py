75400f703bd1041892fffa186e367dd0cff97501
jrpelegrina <juapesai@hotmail.com>
2021-01-25 13:40:12 +0000

    WIP in migration to new n4d

--- n4d-classroom-mountmode.install/usr/share/n4d/python-plugins/ClassroomMountModeManager.py
+++ n4d-classroom-mountmode.install/usr/share/n4d/python-plugins/ClassroomMountModeManager.py
@@ -1,9 +1,13 @@
+#!/usr/bin/python3
 
+import n4d.server.core as n4dcore
+import n4d.responses
 
 class ClassroomMountModeManager:
 	
 	def __init__(self):
 		
+		self.core=n4dcore.Core.get_core()
 		self.lite_status=False
 		self.moving_status=False
 
@@ -18,23 +22,28 @@ class ClassroomMountModeManager:
 	
 	
 	def init_variable(self):
-		
+		'''
 		self.n4d_vars=objects["VariablesManager"]
 		
 		self.lite_status=objects["VariablesManager"].get_variable("SRV_LITE_MODE")
 		self.moving_status=objects["VariablesManager"].get_variable("SRV_MOVING_MODE")
-		
+		'''
+		self.lite_status=self.core.get_variable("SRV_LITE_MODE")["return"]
+		self.moving_status=self.core.get_variable("SRV_MOVING_MODE")["return"]
+
 		
 		if self.lite_status==None:
 			try:
-				self.n4d_vars.add_variable("SRV_LITE_MODE",False,"","Classroom Mount mode configuration","classroom-mount-mode")
+				#Old n4d: self.n4d_vars.add_variable("SRV_LITE_MODE",False,"","Classroom Mount mode configuration","classroom-mount-mode")
+				self.core.set_variable("SRV_LITE_MODE",False)
 			except Exception as e:
 				pass
 			self.lite_status=False
 
 		if self.moving_status==None:
 			try:
-				self.n4d_vars.add_variable("SRV_MOVING_MODE",True,"","Classroom Moving configuration for lite mode","classroom-mount-mode")
+				#Old n4d:self.n4d_vars.add_variable("SRV_MOVING_MODE",True,"","Classroom Moving configuration for lite mode","classroom-mount-mode")
+				self.core.set_variable("SRV_MOVING_MODE",True)
 			except Exception as e:
 				pass
 			self.moving_status=True
@@ -45,14 +54,17 @@ class ClassroomMountModeManager:
 	
 	def is_lite_enabled(self):
 		
-		return {"status":True, "msg":self.lite_status}
+		#Old n4d: return {"status":True, "msg":self.lite_status}
+		return n4d.responses.build_successful_call_response(self.lite_status)
 		
 	#def is_lite_enabled
 	
 	
 	def is_moving_profiles_enabled(self):
 		
-		return {"status":True, "msg":self.moving_status}
+		#Old n4d: return {"status":True, "msg":self.moving_status}
+		return n4d.responses.build_successful_call_response(self.moving_status)
+
 		
 	#def is_lite_enabled
 	
@@ -61,14 +73,20 @@ class ClassroomMountModeManager:
 	
 		if type(value)==bool:
 			try:
-				self.n4d_vars.set_variable("SRV_LITE_MODE",value)
+				#Old n4d: self.n4d_vars.set_variable("SRV_LITE_MODE",value)
+				ret=self.core.set_variable("SRV_LITE_MODE",value)
 				self.lite_status=value
-				return {"status":True, "msg":""}
+				#Old n4d: return {"status":True, "msg":""}
+				return ret
 			except Exception as e:
-				return {"status":False, "msg":str(e)}
+				#Old n4d: return {"status":False, "msg":str(e)}
+				return n4d.responses.build_failed_call_response(str(e))
+				
 				
 		else:
-			return {"status":False, "msg":"Value is not boolean"}
+			#Old n4d: return {"status":False, "msg":"Value is not boolean"}
+			return responses.build_failed_call_response("Value is not boolean")
+
 	
 	#def set_lite_mode
 	
@@ -77,15 +95,20 @@ class ClassroomMountModeManager:
 	
 		if type(value)==bool:
 			try:
-				self.n4d_vars.set_variable("SRV_MOVING_MODE",value)
+				#Old n4d: self.n4d_vars.set_variable("SRV_MOVING_MODE",value)
+				ret=self.core.set_variable("SRV_MOVING_MODE",value)
 				self.moving_status=value
-				return {"status":True, "msg":""}
+				#Old n4d:return {"status":True, "msg":""}
+				return ret
 			except Exception as e:
-				return {"status":False, "msg":str(e)}
+				#Old n4d:return {"status":False, "msg":str(e)}
+				return responses.build_failed_call_response(str(e))
+				
 				
 		else:
-			return {"status":False, "msg":"Value is not boolean"}
-	
+			#Old n4d: return {"status":False, "msg":"Value is not boolean"}
+			return responses.build_failed_call_response("Value is not boolean")
+			
 	#def set_lite_mode
 	
 	
